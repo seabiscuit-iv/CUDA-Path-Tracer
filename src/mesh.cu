2d650e8eaf94a99131a656086d69ad4f5006c0bd
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-10-01 15:21:02 -0400

    bvh construction

--- src/mesh.cu
+++ src/mesh.cu
@@ -33,6 +33,8 @@ void Mesh::make_mesh_device() {
         cudaMemcpy(d_normal_indices, h_normal_indices.data(), num_normal_indices * sizeof(int), cudaMemcpyHostToDevice);
     }
 
+    bvh.make_bvh(h_verts, h_indices);
+
     d_valid = true;
 }
 
@@ -45,5 +47,75 @@ void Mesh::delete_mesh_device() {
         cudaFree(d_normal_indices);
     }
 
+    bvh.delete_bvh();
+
     d_valid = false;
 }
+
+
+void BVH::delete_bvh() {
+    cudaFree(dev_bvh);
+    initizalized = false;
+}
+
+
+// START AND END ARE INCLUSIVE
+// we access indicies with 3 * start or 3 * end
+BoundingBox fill_bvh(int idx, int start, int end, std::vector<BVHNode> &h_bvh, const std::vector<glm::vec3> &verts, const std::vector<int> &indices) {
+    if (start == end) {
+        // base case - single triangle
+        h_bvh[idx] = BVHNode();
+        h_bvh[idx].make_bvh_leaf_node(start);
+        glm::vec3 local_tri[3] = {
+            verts[indices[3 * start]],
+            verts[indices[3 * start + 1]],
+            verts[indices[3 * start + 2]]
+        };
+        return BoundingBox(local_tri, 3);
+    }
+
+    // partition [start_a ... end_a][start_b ... end_b]
+
+    float log = glm::log2(float(end) - float(start));
+    float floor = glm::floor(log);
+    int mid = glm::pow(2.0f, floor);
+
+    int start_a = start;
+    int end_a = start + mid - 1;
+    int start_b = end_a + 1;
+    int end_b = end;
+
+    BoundingBox bbox_a = fill_bvh(LEFT_NODE(idx), start_a, end_a, h_bvh, verts, indices);
+    BoundingBox bbox_b = fill_bvh(RIGHT_NODE(idx), start_b, end_b, h_bvh, verts, indices);
+
+    glm::vec3 combined_bbox[4] = {
+        bbox_a.box_max,
+        bbox_a.box_min,
+        bbox_b.box_max,
+        bbox_b.box_min,
+    };
+
+    h_bvh[idx] = BVHNode();
+    auto fin_bbox = BoundingBox(combined_bbox, 4);
+    h_bvh[idx].make_bvh_node(fin_bbox);
+
+    return fin_bbox;
+}
+
+
+
+void BVH::make_bvh(std::vector<glm::vec3> verts, std::vector<int> indices) {
+    // a binary tree with n leaf nodes must have 2n-1 nodes
+    int num_leafs = indices.size() / 3; // 3 indices per triangle
+    num_nodes = 2 * num_leafs - 1;
+
+    std::vector<BVHNode> h_bvh(num_nodes);
+
+    fill_bvh(0, 0, num_leafs-1, h_bvh, verts, indices);
+
+    cudaMalloc((void**)&dev_bvh, num_nodes * sizeof(BVHNode));
+    cudaMemcpy(dev_bvh, h_bvh.data(), num_nodes * sizeof(BVHNode), cudaMemcpyHostToDevice);
+
+    initizalized = true;
+}
+
--- src/mesh.h
+++ src/mesh.h
@@ -4,6 +4,73 @@
 #include <vector>
 #include <memory>
 
+
+struct BoundingBox {
+    glm::vec3 box_min; // mins of x, y, z
+    glm::vec3 box_max; // maxs of x, y, z
+
+    BoundingBox(glm::vec3 verts[], int length) {
+        glm::vec3 min = verts[0];
+        glm::vec3 max = verts[0];
+
+        for (int i = 1; i < length; i++) {
+            min.x = glm::min(min.x, verts[i].x);
+            min.y = glm::min(min.y, verts[i].y);
+            min.z = glm::min(min.z, verts[i].z);
+
+            max.x = glm::max(max.x, verts[i].x);
+            max.y = glm::max(max.y, verts[i].y);
+            max.z = glm::max(max.z, verts[i].z);
+        }
+
+        box_min = min;
+        box_max = max;
+    }
+
+    BoundingBox() {
+        box_min = glm::vec3(0.0);
+        box_max = glm::vec3(0.0);
+    }
+};
+
+
+
+
+struct BVHNode {
+    bool isLeaf;
+    BoundingBox box; // undefined if isLeaf == true
+    int tri_indices = -1; // indexes into d_indices and d_normal_indices
+
+
+    void make_bvh_node(BoundingBox bbox) {
+        isLeaf = false;
+        box = bbox;
+        tri_indices = -1;
+    }
+
+    void make_bvh_leaf_node(int idx) {
+        isLeaf = true;
+        this->tri_indices = idx;
+    }
+};
+
+
+
+#define LEFT_NODE(x) 2*x + 1
+#define RIGHT_NODE(x) 2*x + 2
+
+struct BVH {
+    bool initizalized = false;
+    int num_nodes;
+    BVHNode* dev_bvh;
+
+    void make_bvh(std::vector<glm::vec3> v, std::vector<int> i);
+    void delete_bvh();
+};
+
+
+
+
 struct Mesh {
     bool h_valid = false;
     bool d_valid = false;
@@ -25,6 +92,8 @@ struct Mesh {
     int* d_indices = nullptr;
     glm::vec3* d_normals = nullptr;
     int* d_normal_indices = nullptr;
+
+    BVH bvh;
 
     void make_mesh_host(std::vector<glm::vec3> v, std::vector<int> i, std::vector<glm::vec3> n, std::vector<int> ni);
     void make_mesh_device();
