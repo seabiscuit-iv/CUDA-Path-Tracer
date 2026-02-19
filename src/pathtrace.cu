10230bd18bcdc580269e403a42f4f0471e9c9e16
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2026-02-19 15:50:51 -0500

    abstract debug mode

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -504,6 +504,39 @@ __device__ glm::vec3 get_normal(const Material& material, const TextureData* tex
 
 
 
+__device__ void render_material_debug_mode(
+    const Material& material, 
+    PathSegment& path, 
+    glm::vec3 materialColor, 
+    glm::vec3 normal, 
+    glm::vec3 normal_map,
+    int idx,
+    int num_paths,
+    int iter,
+    int depth,
+    thrust::default_random_engine& rng
+) {
+    glm::vec3 debug_color = 
+        DEV_OPTIONS.material_debug_mode == 1 ? materialColor :
+        DEV_OPTIONS.material_debug_mode == 2 ? normal :
+        /* DEV_OPTIONS.material_debug_mode == 3 ? */ normal_map;
+
+    if (material.material_type == MaterialType::Emissive && !path.kill) {
+        path.color += path.throughput * material.emittance * material.color;
+        path.kill = true;
+    }
+    else {
+        Lambert::sampleHemisphere(idx, num_paths, iter, depth, path, rng, normal);
+        glm::vec3 lambert = Lambert::shadePathLambert(idx, iter, num_paths, depth, path, material, debug_color, normal, path.sample_dir);
+        float pdf = Lambert::PDF(path.sample_dir, normal);
+        path.throughput *= lambert / pdf;
+    }
+}
+
+
+
+
+
 
 __global__ void shadePath(
     int iter,
@@ -554,21 +587,18 @@ __global__ void shadePath(
         bool is_specular = (material.material_type == MaterialType::Specular || material.material_type == MaterialType::Glass);
 
         if (DEV_OPTIONS.material_debug_mode != 0) {
-            glm::vec3 debug_color = 
-                DEV_OPTIONS.material_debug_mode == 1 ? materialColor :
-                DEV_OPTIONS.material_debug_mode == 2 ? normal :
-                /* DEV_OPTIONS.material_debug_mode == 3 ? */ normal_map;
-
-            if (material.material_type == MaterialType::Emissive && !path.kill) {
-                path.color += path.throughput * material.emittance * material.color;
-                path.kill = true;
-            }
-            else {
-                Lambert::sampleHemisphere(idx, num_paths, iter, depth, path, rng, normal);
-                glm::vec3 lambert = Lambert::shadePathLambert(idx, iter, num_paths, depth, path, material, debug_color, normal, path.sample_dir);
-                float pdf = Lambert::PDF(path.sample_dir, normal);
-                path.throughput *= lambert / pdf;
-            }
+            render_material_debug_mode(
+                material,
+                path,
+                materialColor,
+                normal,
+                normal_map,
+                idx,
+                num_paths,
+                iter,
+                depth,
+                rng
+            );
         }
         else {
             if (material.material_type == MaterialType::Emissive || material.material_type == MaterialType::Diffuse) {
