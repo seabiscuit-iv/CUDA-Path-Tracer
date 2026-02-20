6015f4de79f69a45828af52969dc87946ebfb227
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2026-02-19 21:44:56 -0500

    moved functors to header

--- include/pathtrace.h
+++ include/pathtrace.h
@@ -30,3 +30,49 @@ struct PathTracerOptions {
     PathTracerOptions& operator=(const PathTracerOptions&) = delete;
     PathTracerOptions() = default;
 };
+
+
+
+
+// for stream compaction
+struct path_terminated {
+    __host__ __device__ bool operator()(PathSegment &path) const {
+        return !path.kill;
+    }
+};
+
+struct sort_materials {
+    __host__ __device__ bool operator()(const ShadeableIntersection &sA, const ShadeableIntersection &sB) const {
+        return sA.materialId < sB.materialId;
+    }
+};
+
+struct sort_rays {
+    const Geom* dev_mesh;
+
+    sort_rays(const Geom* dev_geom)
+        : dev_mesh(dev_geom)
+    {}
+
+    __device__ bool operator()(const PathSegment &path) const {
+        Ray r = path.ray;
+
+        r.origin = glm::vec3(dev_mesh->inverseTransform * glm::vec4(r.origin, 1.0f));
+        r.direction = glm::vec3(dev_mesh->inverseTransform * glm::vec4(r.direction, 0.0f));
+
+        #ifdef __CUDA_ARCH__
+            r.inv_direction.x = __frcp_rn(r.direction.x);
+            r.inv_direction.y = __frcp_rn(r.direction.y);
+            r.inv_direction.z = __frcp_rn(r.direction.z);
+        #else
+            r.inv_direction = 1.0f / r.direction;
+        #endif
+
+        r.sign.x = (r.inv_direction.x < 0.0f) ? 1 : 0;
+        r.sign.y = (r.inv_direction.y < 0.0f) ? 1 : 0;
+        r.sign.z = (r.inv_direction.z < 0.0f) ? 1 : 0;
+
+        float t;
+        return dev_mesh->mesh.bvh.dev_bvh[0].box.RayBoxInterection(r, t);
+    }
+};
--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -710,48 +710,6 @@ __global__ void finalGather(int nPaths, glm::vec3* image, PathSegment* __restric
     }
 }
 
-
-// for stream compaction
-struct path_terminated {
-    __host__ __device__ bool operator()(PathSegment &path) const {
-        return !path.kill;
-    }
-};
-
-struct sort_materials {
-    __host__ __device__ bool operator()(const ShadeableIntersection &sA, const ShadeableIntersection &sB) const {
-        return sA.materialId < sB.materialId;
-    }
-};
-
-
-struct sort_rays {
-    const Geom* dev_mesh;
-
-    sort_rays(const Geom* dev_geom)
-        : dev_mesh(dev_geom)
-    {}
-
-    __device__ bool operator()(const PathSegment &path) const {
-        Ray r = path.ray;
-
-        r.origin = glm::vec3(dev_mesh->inverseTransform * glm::vec4(r.origin, 1.0f));
-        r.direction = glm::vec3(dev_mesh->inverseTransform * glm::vec4(r.direction, 0.0f));
-
-        r.inv_direction.x = __frcp_rn(r.direction.x);
-        r.inv_direction.y = __frcp_rn(r.direction.y);
-        r.inv_direction.z = __frcp_rn(r.direction.z);
-
-        r.sign.x = (r.inv_direction.x < 0.0f) ? 1 : 0;
-        r.sign.y = (r.inv_direction.y < 0.0f) ? 1 : 0;
-        r.sign.z = (r.inv_direction.z < 0.0f) ? 1 : 0;
-
-        float t;
-        return dev_mesh->mesh.bvh.dev_bvh[0].box.RayBoxInterection(r, t);
-    }
-};
-
-
 #define MORTON_INTERP_DIST 1.0f
 
 __device__ inline uint32_t expandBits(uint32_t v) {
